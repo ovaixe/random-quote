c0ef5aa057b2c13ba1fcf8c17fa5c22185adee25
ovaixe <owaisbhat996@outlook.com>
2022-05-25 23:16:10 +0530

    changed setup configuration

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rquote-ovaixe",
+    version="0.0.1",
+    author="Bhat Owais",
+    author_email="owaisbhat996@outlook.com",
+    description="A simple package which gets a random quote",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ovaixe/random-quote",
+    project_urls={
+        "Bug Tracker": "https://github.com/ovaixe/random-quote/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=['requests'],
+)
+
